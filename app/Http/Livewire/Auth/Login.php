3a7a957030861bd96a7fd5f670d23c4ecd672ac1
Kelvin <kelvinvip8@gmail.com>
2020-06-20 23:00:26 +0100

    WIP: Logoin

--- app/Http/Livewire/Auth/Login.php
+++ app/Http/Livewire/Auth/Login.php
@@ -6,6 +6,29 @@ use Livewire\Component;
 
 class Login extends Component
 {
+    public $email;
+    public $password;
+    public $remember;
+
+    /**
+     * Action on form submit
+     *
+     * @return void
+     */
+    public function submit()
+    {
+        $this->validate([
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'min:8']
+        ]);
+
+        $auth = auth()->attempt(['email' => $this->email, 'password' => $this->password], $this->remember);
+
+        return $auth
+            ? redirect(route('home'))
+            : redirect()->back()->with('error', 'The credentials do not exist in our database');
+    }
+
     public function render()
     {
         return view('livewire.auth.login');
--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,3 +1,83 @@
-<div>
-    {{-- The whole world belongs to you --}}
-</div>
+<section>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Login') }}</div>
+
+                    <div class="card-body">
+                        @if ( session('error'))
+                        <div class="alert alert-danger">
+                            {{ session('error') }}
+                        </div>
+                        @endif
+                        <form wire:submit.prevent="submit">
+                            @csrf
+
+                            <div class="form-group row">
+                                <label for="email"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="email" type="email"
+                                        class="form-control @error('email') is-invalid @enderror"
+                                        wire:model.lazy="email" required autocomplete="off" value="{{ old('email') }}">
+
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="password"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="password" type="password"
+                                        class="form-control @error('password') is-invalid @enderror"
+                                        wire:model.lazy="password" required>
+
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <div class="col-md-6 offset-md-4">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" wire:model.lazy="remember"
+                                            id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                        <label class="form-check-label" for="remember">
+                                            {{ __('Remember Me') }}
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Login') }}
+                                    </button>
+
+                                    @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                    @endif
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
